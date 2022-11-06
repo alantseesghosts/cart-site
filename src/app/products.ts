9378bc67431556fe8ff8322ee42c5d9f0c0abc92
Alant Wilson <52749543+alantseesghosts@users.noreply.github.com>
2022-11-06 17:04:12 -0500

    Add files via upload

---
+++ src/app/products.ts
@@ -0,0 +1,16 @@
+
+
+export interface Product {
+  number: number;
+  name: string;
+  price: number;
+  size: string;
+  color: string;
+  quantity: number;
+}
+
+export const products = [
+  {"number":3,"name":"Jack Johnson","price":100.0,"size":"L","color":"blue","quantity":20},{"number":100,"name":"Alexandar Georgiev","price":100.0,"size":"L","color":"blue","quantity":20},{"number":6,"name":"Erik Johnson","price":100.0,"size":"L","color":"blue","quantity":20},{"number":8,"name":"Cale Makar","price":100.0,"size":"L","color":"blue","quantity":20},{"number":40,"name":"Alexandar Georgiev","price":100.0,"size":"L","color":"blue","quantity":20},{"number":25,"name":"Logan O'Connor","price":100.0,"size":"L","color":"blue","quantity":20},{"number":10,"name":"Alexandar Georgiev","price":100.0,"size":"L","color":"blue","quantity":20},{"number":95,"name":"Andre Burakovsky","price":4000.0,"size":"L","color":"blue","quantity":20}]
+;
+
+
